c215940df6def3270aaaf0d20c3d4d8709112ffe
1MbeweWhitecrow2 <przemyslaw.mariusz.mitura@gmail.com>
2024-10-02 19:02:20 +0100

    modified function get_books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,9 +11,21 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+function getBooks() {
+  return new Promise((resolve, reject) => {
+      resolve(books);
+  });
+}
+
+// Get the book list available in the shop
+public_users.get('/', async function (req, res) {
+  try {
+    const bks = await getBooks();
+    res.send(JSON.stringify(bks));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // Get book details based on ISBN
